3f087dc69b92b40bb5d5df42e9ff80aaf6fdff33
Amnexico <63729452+Amnexico@users.noreply.github.com>
2025-08-20 01:02:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,28 +5,24 @@ const database = require('./lib/database');
 const Bot = require('./lib/bot');
 const priceTracker = require('./lib/price-tracker');
 const Alert = require('./lib/templates/alert');
-
 const mongoConnectionURI = config.get('mongo.connectionURI');
 const telegramBotToken = config.get('telegram.token');
-
 const bot = new Bot(telegramBotToken);
 
 database.connect(mongoConnectionURI).then(() => {
   bot.launch();
   priceTracker.start();
-
   priceTracker.on('update', product => {
-  const alert = new Alert(product).toMarkdown();
+    const alert = new Alert(product).toMarkdown();
+    
+    if (alert.extra) {
+      bot.sendMessage(product.user, alert.text, alert.extra);
+    } else {
+      bot.sendMessage(product.user, alert);
+    }
+    
+    logger.info(`Alert sent: ${product.name} (${product.id})`);
+  });
   
-  if (alert.extra) {
-    // Mensaje con botón inline
-    bot.sendMessage(product.user, alert.text, alert.extra);
-  } else {
-    // Mensaje simple
-    bot.sendMessage(product.user, alert);
-  }
-  
-  logger.info(`Alert sent: ${product.name} (${product.id})`);
+  logger.info('Pricegram started...');
 });
-
-
